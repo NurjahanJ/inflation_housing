37a2d39b653549c38af71ef4802333e11fb9a87c
Nurjahan Jhorna <nj32@njit.edu>
2025-02-23 14:58:28 -0500

    Repostioned the line chart and heat map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // src/App.js
 import React from 'react';
 import LineChart from './LineChart';
-import ScatterPlot from './ScatterPlot';
 import HeatMap from './HeatMap';
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ textAlign: 'center' }}>Housing and Inflation Analysis</h1>
-      <LineChart />
-      <ScatterPlot />
-      <HeatMap />
+    <div className="charts-wrapper">
+      <div className="chart-container">
+        <LineChart />
+      </div>
+      <div className="heatmap-container">
+        <HeatMap />
+      </div>
     </div>
   );
 }
